c3cc8f4fb032d4a9304578c7e07ebc10f8c596e3
许兴伦 <xuxinglun@xuxingluns-MacBook-Pro.local>
2017-07-02 17:53:28 +0800

    Initial Commit

---
+++ Halo_Engine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Halo_Engine
+//
+//  Created by 许兴伦 on 7/2/17.
+//  Copyright © 2017 SparkLight. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
