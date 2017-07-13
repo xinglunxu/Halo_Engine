9f854df0cea3ae29a79af702ac170f17041a4276
许兴伦 <xinglunxu@gmail.com>
2017-07-13 14:45:06 +0800

    Delete unnecessary code

--- MovingSystem.hpp
+++ MovingSystem.hpp
@@ -26,34 +26,9 @@ public:
     std::list<std::type_index>* GetDataTypes() override;
     void InjectData(std::list<void*> *datas) override;
     MovingSystem();
-    template <typename ... types> void InjectDatas(std::unordered_map<std::type_index, void*>* datas){
-        _InjectDatas<types... >(datas);
-    }
-    
 private:
     std::unordered_map<int, Transform*>* transforms;
     std::unordered_map<int, Velocity*>* velocities;
-    
-    template <typename T, typename ... types> void _InjectDatas(std::unordered_map<std::type_index, void*>* datas){
-        void* dataP = (*datas)[std::type_index(typeid(T))];
-        std::unordered_map<int, T*>* data = static_cast<std::unordered_map<int, T*>*>(dataP);
-    }
-    
-    
-};
-
-template <typename ... types>
-struct InjectDataClass{
-public:
-    static void call(std::unordered_map<std::type_index, void*>* datas,MovingSystem* ms){
-        ms->_InjectDatas<types...>(datas,ms);
-    }
-};
-
-template <>
-struct InjectDataClass<>{
-public:
-    static void call(std::unordered_map<std::type_index, void*>* datas,MovingSystem* ms){}
 };
 
 #endif /* MovingSystem_hpp */
